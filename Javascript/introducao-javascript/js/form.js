cd645189ba647ab871df24fa122b74e6ea06097c
Bruno Ramos <brunoramos90gmail.com>
2019-04-15 22:50:07 -0300

    [ALURA] Cap 09 Jscript (FInal)

--- Javascript/introducao-javascript/js/form.js
+++ Javascript/introducao-javascript/js/form.js
@@ -4,23 +4,28 @@ botaoAdicionar.addEventListener("click", function(event){
 
   var form = document.querySelector("#form-adiciona");
   var paciente = obtemPacienteDoFormulario(form);
-  var pacienteTr = montaTr(paciente);
 
   var erros = validaPaciente(paciente);
-
-  if (erros.length > 0){
+  console.log(erros);
+  if (erros.length > 0) {
     exibeMensagensDeErro(erros);
     return;
   }
 
-  var tabela = document.querySelector("#tabela-pacientes");
-  tabela.appendChild(pacienteTr);
+  adicionaPacienteNaTabela(paciente);
 
   form.reset();
+
   var mensagensErro = document.querySelector("#mensagens-erro");
-  mensagensErro.innerHTML = ""
+  mensagensErro.innerHTML = "";
 
 });
+
+function adicionaPacienteNaTabela(paciente) {
+  var pacienteTr = montaTr(paciente);
+  var tabela = document.querySelector("#tabela-pacientes");
+  tabela.appendChild(pacienteTr);
+}
 
 function exibeMensagensDeErro(erros){
   var ul = document.querySelector("#mensagens-erro");
